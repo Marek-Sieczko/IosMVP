536d85c3c1f1628b55ccb45bab97fe22a6b474de
mdo91 <hesoka_mxx@yahoo.com>
2020-08-21 14:39:40 +0300

    adding details view controller class

---
+++ mvpDesginPatterm/UI/Details/SecretDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecretDetailsViewController.swift
+//  mvpDesginPatterm
+//
+//  Created by Mdo on 21/08/2020.
+//  Copyright © 2020 Mdo. All rights reserved.
+//
+
+import UIKit
+
+class SecretDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
